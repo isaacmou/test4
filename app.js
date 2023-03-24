11e32e13f88fd54c62a8c3d0429c0094bcd18ddc
isaacmou <126214425+isaacmou@users.noreply.github.com>
2023-03-24 17:33:12 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,40 @@ function showEnglish() {
     document.getElementById("engInput").style.display = "";
     document.getElementById("chiInput").style.display = "none";
     document.getElementById("engVerwithdistrict").style.display = "none";
+    <!--Show english button-->
+    document.getElementById("btn1").style.display = "";
+    document.getElementById("btn2").style.display = "";
+    document.getElementById("btn3").style.display = "";
+    document.getElementById("btn4").style.display = "";
+    document.getElementById("btn5").style.display = "";
+    document.getElementById("btn6").style.display = "";
+    document.getElementById("btn7").style.display = "";
+    document.getElementById("btn8").style.display = "";
+    document.getElementById("btn9").style.display = "";
+    document.getElementById("btn10").style.display = "";
+    document.getElementById("btn11").style.display = "";
+    document.getElementById("btn12").style.display = "";
+    document.getElementById("btn13").style.display = "";
+    document.getElementById("btn14").style.display = "";
+    document.getElementById("btn15").style.display = "";
+    document.getElementById("btn16").style.display = "";
+    document.getElementById("btn17").style.display = "";
+    document.getElementById("btn18").style.display = "";
+    document.getElementById("btn19").style.display = "";
+    document.getElementById("btn20").style.display = "";
+    document.getElementById("btn21").style.display = "";
+    document.getElementById("btn22").style.display = "";
+    document.getElementById("btn23").style.display = "";
+    document.getElementById("btn24").style.display = "";
+    document.getElementById("btn25").style.display = "";
+    document.getElementById("btn26").style.display = "";
+    document.getElementById("btn27").style.display = "";
+    document.getElementById("btn28").style.display = "";
+    document.getElementById("btn29").style.display = "";
+    document.getElementById("btn30").style.display = "";
+    document.getElementById("btn31").style.display = "";
+    
+    
 
 }
 
