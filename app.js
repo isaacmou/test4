be3e9a336c43b87ea3b9a87550876dcfa7566ec6
isaacmou <126214425+isaacmou@users.noreply.github.com>
2023-03-24 17:46:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -24,23 +24,24 @@ function showEnglish() {
     document.getElementById("btn16").style.display = "";
     document.getElementById("btn17").style.display = "";
     document.getElementById("btn18").style.display = "";
-    document.getElementById("btn19").style.display = "";
-    document.getElementById("btn20").style.display = "";
-    document.getElementById("btn21").style.display = "";
-    document.getElementById("btn22").style.display = "";
-    document.getElementById("btn23").style.display = "";
-    document.getElementById("btn24").style.display = "";
-    document.getElementById("btn25").style.display = "";
-    document.getElementById("btn26").style.display = "";
-    document.getElementById("btn27").style.display = "";
-    document.getElementById("btn28").style.display = "";
-    document.getElementById("btn29").style.display = "";
-    document.getElementById("btn30").style.display = "";
-    document.getElementById("btn31").style.display = "";
-    document.getElementById("btn32").style.display = "";
-    document.getElementById("btn33").style.display = "";
-    document.getElementById("btn34").style.display = "";
-    document.getElementById("btn35").style.display = "";
+    document.getElementById("btn19").style.display = "none";
+    document.getElementById("btn20").style.display = "none";
+    document.getElementById("btn21").style.display = "none";
+    document.getElementById("btn22").style.display = "none";
+    document.getElementById("btn23").style.display = "none";
+    document.getElementById("btn24").style.display = "none";
+    document.getElementById("btn25").style.display = "none";
+    document.getElementById("btn26").style.display = "none";
+    document.getElementById("btn27").style.display = "none";
+    document.getElementById("btn28").style.display = "none";
+    document.getElementById("btn29").style.display = "none";
+    document.getElementById("btn30").style.display = "none";
+    document.getElementById("btn31").style.display = "none";
+    document.getElementById("btn32").style.display = "none";
+    document.getElementById("btn33").style.display = "none";
+    document.getElementById("btn34").style.display = "none";
+    document.getElementById("btn35").style.display = "none";
+    document.getElementById("btn36").style.display = "none";
     
 
 }
